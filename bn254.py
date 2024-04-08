f589a1836eb582fb3b8e15b9db727e9e228abff0
Theo <96906674+theo-for-you@users.noreply.github.com>
2024-04-08 14:14:23 +0000

    Add files via upload

---
+++ bn254.py
@@ -0,0 +1,42 @@
+def legendre(a, p):
+    return pow(a, (p - 1) // 2, p)
+
+def tonelli(n, p):
+    assert legendre(n, p) == 1, "not a square (mod p)"
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q //= 2
+        s += 1
+    if s == 1:
+        return pow(n, (p + 1) // 4, p)
+    for z in range(2, p):
+        if p - 1 == legendre(z, p):
+            break
+    c = pow(z, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
+    m = s
+    t2 = 0
+    while (t - 1) % p != 0:
+        t2 = (t * t) % p
+        for i in range(1, m):
+            if (t2 - 1) % p == 0:
+                break
+            t2 = (t2 * t2) % p
+        b = pow(c, 1 << (m - i - 1), p)
+        r = (r * b) % p
+        c = (b * b) % p
+        t = (t * c) % p
+        m = i
+    return r
+
+
+p = 21888242871839275222246405745257275088696311157297823662689037894645226208583
+x = int("0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16) % p
+
+
+y = tonelli((x**3 + 3) % p, p)
+print('y=', y)
+print('x=', x)
+print((y ** 2) % p == (x**3 + 3) % p)
